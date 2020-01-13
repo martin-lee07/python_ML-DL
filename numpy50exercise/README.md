79509be1df99c83434e6fd573ba63d07b6123d8f
Your Name <email@example.com>
2020-01-13 16:57:52 +0800

    --

---
+++ numpy50exercise/README.md
@@ -0,0 +1,1 @@
+#this project is Chinese version of the project https://github.com/rougier/numpy-100
--- numpy50exercise/numpy50exercise.py
+++ numpy50exercise/numpy50exercise.py
@@ -1,30 +1,30 @@
 #1.导入numpy包命名为np
 import numpy as np
 
-#打印numpy版本和配置
+#2.打印numpy版本和配置
 print(np.__version__)
 np.show_config()
 
-#创建一个10*10的空数组
+#3.创建一个10*10的空数组
 z = np.zeros(10)
 print(z)
 
-#查看数组内存大小
+#4.查看数组内存大小
 z = np.zeros((10, 10))
 print(z)
 print(z.size)
 print(z.itemsize)
 print("%d bytes" % (z.size * z.itemsize))
 
-#在命令行下查看numpy add函数的文档
+#5.在命令行下查看numpy add函数的文档
 #%run `python -c "import numpy;numpy.info(numpy.add)"`
 
-#创建一个长度为10的0数组，第5个数值为1
+#6.创建一个长度为10的0数组，第5个数值为1
 z = np.zeros(10)
 z[4] = 1
 print(z)
 
-#创建一个值从1到49的数组
+#7.创建一个值从1到49的数组
 z = np.arange(10, 50)
 print(z)
 
@@ -37,7 +37,7 @@ print(z)
 z = np.arange(9).reshape(3, 3)
 print(z)
 
-nz = np.nonzero([1, 2, 0, 0, 4, 0])
+nz = np.nonzero((1, 2, 0, 0, 4, 0))
 #从[1,2,0,0,4,0]中找到
 #包装成ndarray
 # nz=np.array(nz,dtype=float)
@@ -76,11 +76,73 @@ print(0*np.nan)#nan
 print(np.nan == np.nan)  #False
 print(np.inf>np.nan)#False
 print(np.nan-np.nan)#nan
-print(np.nan in set([np.nan]))#True     
+print(np.nan in set([np.nan]))#True
 print(0.3 == 0.3 * 1)  #True
 
 #创建一个5*5矩阵，对角线下方值为1，2，3，4
 z=np.diag(1+np.arange(4),k=-1)
 print(z)
 
-#
+#创建一个8*8矩阵，并用棋盘图案填充
+z=np.zeros((8,8),dtype=int)
+z[1::2,::2] = 1
+z[::2, 1::2] = 1
+print(z)
+#广播机制
+# for i in range(len(z)):
+#     z[i]=z[i]+1
+# print(z)
+
+#给定一个（6，7，8）大小的三维矩阵，求100个元素的索引是什么
+print(np.unravel_index(99,(6,7,8)))
+
+#使用一个tile函数创建8*8的棋盘矩阵
+#tile(A,resp)函数：A为原矩阵，resp为每个维度的重复次数
+z=np.tile(np.array([[0,1],[1,0]]),(4,4))
+print(z)
+
+#对一个5*5矩阵进行标准化处理
+#标准化：原值-平均值/方差
+z=np.random.random((5,5))
+z=((z-np.mean(z))/(np.std(z)))
+print(z)
+
+#新建立一个dtype类型用来描述一个颜色（RGBA）
+color=np.dtype([("r",np.ubyte,1),("g",np.ubyte,1),("b",np.ubyte,1),("a",np.ubyte,1)])
+print(color)
+
+#5*3矩阵和3*2矩阵相乘
+z=np.dot(np.ones((5,3)),np.ones((3,2)))
+print(z)
+
+z=np.ones((5,3))@np.ones((3,2))
+print(z)
+
+#给定一个一维数组，将3~8中间元素取反
+z=np.arange(10)
+z[(3<z)&(z<8)]*=-1
+print(z)
+
+#看看下列脚本会输出什么？
+print(sum(range(5),-1))
+from numpy  import *
+print(sum(range(5),-1))
+
+#给定一个整数数组z，看看下面哪个表达式是合法的？
+z=[1,2,3,4]
+# print(z**z)
+# 2<<z>>2
+# z<-z
+# 1j*z
+# z/1/1
+# z<z>z
+
+#下面表达式的结果是什么？
+print(np.array(0)/np.array(0))#nan
+print(np.array(0)//np.array(0))#0
+print(np.array([np.nan]).astype(int).astype(float))#[-2.14748365e+09]
+
+#np.random.uniform已...为分布采样
+z=np.random.uniform(-10,+10,10)
+print(z)
+print(np.copysign(np.ceil(np.abs(z)),z))
